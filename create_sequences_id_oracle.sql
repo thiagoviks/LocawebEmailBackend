1612546f7b97ce6af464204339b40ed981d3b999
Thiago Silva <62107168+thiagoviks@users.noreply.github.com>
2024-10-16 23:34:54 -0300

    Oracle Database SQL code and MER diagram

---
+++ create_sequences_id_oracle.sql
@@ -0,0 +1,111 @@
+CREATE SEQUENCE usuario_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE contato_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+
+CREATE SEQUENCE email_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE evento_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+
+CREATE SEQUENCE pasta_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE anexo_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE rascunho_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE regras_filtragem_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE condicoes_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE acoes_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE configuracao_usuario_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE categoria_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	 
+CREATE SEQUENCE rotulo_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
+	  
+CREATE SEQUENCE preferencia_usuario_seq
+      INCREMENT BY 1
+      NOMINVALUE
+      NOMAXVALUE
+      START WITH 1
+      NOCACHE
+      NOCYCLE;
